f34b2ef228351b47d5ec19634972649a62e4b873
Peter Palka <ppalka@cmcrc.com>
2018-05-30 15:30:45 +1000

    Some script fixtures and Readme update about jq installation

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 Simple script to download latest Bing picture, set it as Ubuntu wallpaper and
 lock screen. Tested on Ubuntu 17.10
 
+## Dependencies
+Among standard utilities script use "jq" command so before execution please make sure that it is available.
+```
+install jq
+```
+Also, if you are going to run it with Cron make sure that Cron can see it.
+
 ## Usage
 Just run the script. It will be executed once and quit. You can schedule it with
-cron. This example shows how to execute it every 10 mins:
+Cron. This example shows how to execute it every 10 mins:
 
 ```
 */10 0 * * * bash /path/to/wallpaper-changer.sh
--- wallpaper-changer.sh
+++ wallpaper-changer.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 #
 # Downoad latest Bing picture. set random picture to wallpaper and lock screen
+# This script is designed to run with Cron (but can be executed manually, too)
 #
 
 # CONSTS
 #
 # Cron has no $USER env var so I need to use 'whoami' command
 WALLPAPER_PATH="/home/$(whoami)/Pictures/Wallpapers"
+# Add snap binaries to PATH (jq can be installed there) as normally cron has only PATH=/usr/bin
+PATH="$PATH:/snap/bin"
 
 # Need to export current dbus session bus address. Other way gsettings won't work
 export $(grep -z "DBUS_SESSION_BUS_ADDRESS" /proc/$(pgrep -n gnome-session)/environ)
@@ -14,6 +17,7 @@ export $(grep -z "DBUS_SESSION_BUS_ADDRESS" /proc/$(pgrep -n gnome-session)/envi
 # Check if dir to store wallpapers exists
 if [ ! -d "$WALLPAPER_PATH" ]; then
     mkdir -p "$WALLPAPER_PATH"
+    # TODO: Download some latest wallpapers from Bing
 fi
 
 cd "$WALLPAPER_PATH"
@@ -25,9 +29,9 @@ cd "$WALLPAPER_PATH"
 # n - number of things to display. Available values: 0-8 (zero displays nothing).
 # mkt - localization. Looks like Bing sets wallpapers depends on what language do you use.
 #       Tested values that works: en-US, en-AU, pl-PL
-wget --quiet --no-clobber https://www.bing.com"$(wget --quiet -O-  'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pl-PL' | jq -r .images[0].url)"
+wget --quiet --no-clobber "https://www.bing.com$(wget --quiet -O-  'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-AU' | jq -r .images[0].url)"
 
-# Leaving commented echoes as I'm still testing it if DBUS_SESSION_BUS_ADDRESS env works 
+# Leaving commented echoes as I'm still testing it if DBUS_SESSION_BUS_ADDRESS env works
 
 # Get random wallpaper picture
 wallpaper_uri="file://$(find $WALLPAPER_PATH -type f -name \*.jpg | sort --random-sort | head -n 1)"
