eb8530018b72c650e692213ab1eda5009baed298
Peter Palka <ppalka@cmcrc.com>
2018-05-30 11:57:49 +1000

    Fixed gsettings problem with DBUS_SESSION_BUS_ADDRESS env var set up

--- wallpaper-changer.sh
+++ wallpaper-changer.sh
@@ -3,8 +3,13 @@
 # Downoad latest Bing picture. set random picture to wallpaper and lock screen
 #
 
+# CONSTS
+#
 # Cron has no $USER env var so I need to use 'whoami' command
 WALLPAPER_PATH="/home/$(whoami)/Pictures/Wallpapers"
+
+# Need to export current dbus session bus address. Other way gsettings won't work
+export $(grep -z "DBUS_SESSION_BUS_ADDRESS" /proc/$(pgrep -n gnome-session)/environ)
 
 # Check if dir to store wallpapers exists
 if [ ! -d "$WALLPAPER_PATH" ]; then
@@ -22,16 +27,16 @@ cd "$WALLPAPER_PATH"
 #       Tested values that works: en-US, en-AU, pl-PL
 wget --quiet --no-clobber https://www.bing.com"$(wget --quiet -O-  'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pl-PL' | jq -r .images[0].url)"
 
+# Leaving commented echoes as I'm still testing it if DBUS_SESSION_BUS_ADDRESS env works 
+
 # Get random wallpaper picture
 wallpaper_uri="file://$(find $WALLPAPER_PATH -type f -name \*.jpg | sort --random-sort | head -n 1)"
-echo "Next wallpaper will be: $wallpaper_uri"
+#echo "Next wallpaper will be: $wallpaper_uri"
 
 # Set background
-# there's a problem with setting background (it's not being set up), need to figure out why
 gsettings set org.gnome.desktop.background picture-uri "$wallpaper_uri"
-echo "Current background: $(gsettings get org.gnome.desktop.background picture-uri)"
+#echo "Current background: $(gsettings get org.gnome.desktop.background picture-uri)"
 
 # Set lock screen
-# there's a problem, too
 gsettings set org.gnome.desktop.screensaver picture-uri "$wallpaper_uri"
-echo "Current lock screen: $(gsettings get org.gnome.desktop.screensaver picture-uri)"
+#echo "Current lock screen: $(gsettings get org.gnome.desktop.screensaver picture-uri)"
