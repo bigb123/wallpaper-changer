b14c2790c6d52ef7a6b6363c4d6d241103b8102f
Peter Palka <ppalka@cmcrc.com>
2018-05-30 10:56:42 +1000

    Fixed path. Need to fix background set up

--- wallpaper-changer.sh
+++ wallpaper-changer.sh
@@ -3,7 +3,8 @@
 # Downoad latest Bing picture. set random picture to wallpaper and lock screen
 #
 
-WALLPAPER_PATH="/home/$USER/Pictures/Wallpapers"
+# Cron has no $USER env var so I need to use 'whoami' command
+WALLPAPER_PATH="/home/$(whoami)/Pictures/Wallpapers"
 
 # Check if dir to store wallpapers exists
 if [ ! -d "$WALLPAPER_PATH" ]; then
@@ -19,13 +20,18 @@ cd "$WALLPAPER_PATH"
 # n - number of things to display. Available values: 0-8 (zero displays nothing).
 # mkt - localization. Looks like Bing sets wallpapers depends on what language do you use.
 #       Tested values that works: en-US, en-AU, pl-PL
-wget --quiet --no-clobber https://www.bing.com"$(wget --quiet -O-  'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-AU' | jq -r .images[0].url)"
+wget --quiet --no-clobber https://www.bing.com"$(wget --quiet -O-  'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pl-PL' | jq -r .images[0].url)"
 
 # Get random wallpaper picture
 wallpaper_uri="file://$(find $WALLPAPER_PATH -type f -name \*.jpg | sort --random-sort | head -n 1)"
+echo "Next wallpaper will be: $wallpaper_uri"
 
 # Set background
+# there's a problem with setting background (it's not being set up), need to figure out why
 gsettings set org.gnome.desktop.background picture-uri "$wallpaper_uri"
+echo "Current background: $(gsettings get org.gnome.desktop.background picture-uri)"
 
 # Set lock screen
+# there's a problem, too
 gsettings set org.gnome.desktop.screensaver picture-uri "$wallpaper_uri"
+echo "Current lock screen: $(gsettings get org.gnome.desktop.screensaver picture-uri)"
