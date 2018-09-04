872721c012f56f49d3ddcc0e4dfd9bab714c2146
Peter Palka <ppalka@cmcrc.com>
2018-09-04 17:34:18 +1000

    Archiving pictures rather than removing it

--- wallpaper-changer.sh
+++ wallpaper-changer.sh
@@ -31,30 +31,31 @@ if [ ! -d "$WALLPAPER_PATH" ]; then
 fi
 
 cd "$WALLPAPER_PATH" || exit 1
-# Remove oldest pictures and keep just a bunch of them
+# Move oldest pictures to Archive and keep just a bunch of them as active.
 number_of_wallpapers_to_keep=30
 number_of_current_wallpaper=0
-for wallpaper in $(ls -t); do
+mkdir -p "Archive"
+for wallpaper in $(ls -t *.jpg); do
   if [ $number_of_current_wallpaper -lt $number_of_wallpapers_to_keep ]; then
     # Keep the wallpaper
     number_of_current_wallpaper=$(($number_of_current_wallpaper+1))
   else
     # Remove wallpaper
-    rm "$wallpaper"
+    mv "$wallpaper" Archive/
   fi
 done
 
-# Checkout latest picture (skip if exist <- no-clobber option)
+# Checkout latest picture (skip if exist -> no-clobber option)
 #
 # Bing api https link arguments useful info:
 # idx - index number of item to display as first. Available values: 0-7.
 # n - number of things to display. Available values: 0-8 (zero displays nothing).
 # mkt - localization (market). Looks like Bing sets wallpapers depends on what
 #       language do you use. Tested values that works: en-US, en-AU, pl-PL.
-wget --quiet --no-clobber "https://www.bing.com$(wget --quiet -O-  'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-AU' | jq -r .images[0].url)"
+wget --quiet --no-clobber "https://www.bing.com$(wget --quiet -O- 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-AU' | jq -r .images[0].url)"
 
 # Get random wallpaper picture
-wallpaper_uri="file://$(find $WALLPAPER_PATH -type f -name \*.jpg | sort --random-sort | head -n 1)"
+wallpaper_uri="file://$(find $WALLPAPER_PATH -maxdepth 1 -type f -name \*.jpg | sort --random-sort | head -n 1)"
 
 # Set background
 gsettings set org.gnome.desktop.background picture-uri "$wallpaper_uri"
